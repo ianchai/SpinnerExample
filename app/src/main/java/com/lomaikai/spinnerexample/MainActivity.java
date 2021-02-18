72719b427169b218bcb4cf35f963940c4b4ad01b
Ian Chai <ianchai@mmu.edu.my>
2021-02-18 12:43:50 +0800

    Spinner example completed with hard-coded items

--- app/src/main/java/com/lomaikai/spinnerexample/MainActivity.java
+++ app/src/main/java/com/lomaikai/spinnerexample/MainActivity.java
@@ -3,7 +3,11 @@ package com.lomaikai.spinnerexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -25,5 +29,25 @@ public class MainActivity extends AppCompatActivity {
         carArrayList.add("Proton");
         carArrayList.add("Perodua");
         carArrayList.add("BMW");
+
+        ArrayAdapter<String> carAdapter = new ArrayAdapter(
+                this,
+                android.R.layout.simple_spinner_dropdown_item,
+                carArrayList
+        );
+
+        carSpinner.setAdapter(carAdapter);
+
+        carSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, carArrayList.get(i)+" selected", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 }
